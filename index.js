6dc79becc131cd91aa6209c6b6fdcc228c272d10
Luis Angel <angelfgdeveloper@gmail.com>
2023-04-11 09:44:46 -0600

    Configuracion básica en express

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+
+// Puerto
+const PORT = process.env.PORT;
 
 // Crear el servidor de express
 const app = express();
 
+// Directorio Público
+app.use(express.static('public'));
+
 // Rutas
-app.get('/', (req, res) => {
-  res.status(200).json({
-    ok: true
-  });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     ok: true
+//   });
+// });
 
 // Escuchar peticiones
-app.listen(4000, () => {
-  console.log(`Servidor corriendo en puerto ${ 4000 }`);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${ PORT }`);
+  console.log(`http://localhost:${ PORT }`);
 });
