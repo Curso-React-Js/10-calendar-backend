82acc61113af71cefdeea9ba5c5093e1809a7056
Luis Angel <angelfgdeveloper@gmail.com>
2023-04-12 13:27:30 -0600

    Creacion de un usuario en DB con hash bcryptjs en password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,41 @@
 const { request, response } = require('express');
-const { validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User');
 
-const createUser = (req = request, res = response) => {
+const createUser = async (req = request, res = response) => {
+  const { email, password } = req.body;
 
-  const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        pk: false,
+        msg: 'Ya existe un usuario con ese correo'
+      });
+    }
 
-  res.status(201).json({
-    ok: true,
-    msg: 'registro',
-    name,
-    email,
-    password
-  });
+    user = new User(req.body);
+
+    // Encriptar contraseña
+    const salt = bcryptjs.genSaltSync();
+    user.password = bcryptjs.hashSync(password, salt);
+
+    await user.save();
+  
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+      email: user.email
+    });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    });
+  }
 
 }
 
